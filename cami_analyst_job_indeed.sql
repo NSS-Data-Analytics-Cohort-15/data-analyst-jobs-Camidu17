666c55c12f3e74f17c46298b5fc3324f3e842e69
Camidu17 <mariadu0217@gmail.com>
2025-06-07 10:33:37 -0500

    Done

--- cami_analyst_job_indeed.sql
+++ cami_analyst_job_indeed.sql
@@ -67,10 +67,10 @@ WHERE location = 'CA';
 --Question 9 Find the name of each company and its average star rating for all companies that have more than 5000 reviews across all locations. 
 SELECT company, AVG(star_rating) AS avg_rating
 FROM data_analyst_jobs_indeed
-WHERE review_count > 5000
+WHERE review_count > 5000 AND company IS NOT NULL
 GROUP BY company;
 
--- ANSWER: 
+-- ANSWER: 40
 "Comcast"
 "Microsoft"
 "Nordstrom"
@@ -112,13 +112,58 @@ GROUP BY company;
 "Capgemini"
 "Meijer"
 
---How many companies are there with more that 5000 reviews across all locations?
-SELECT count(company)
-FROM data_analyst_jobs_indeed
-WHERE review_count > 5000;
-
--- ANSWER:184
-
 -- Question 10 Add the code to order the query in #9 from highest to lowest average star rating. 
 -- Which company with more than 5000 reviews across all locations in the dataset has the highest star rating? What is that rating?
+
+SELECT company, AVG(star_rating) AS avg_rating
+FROM data_analyst_jobs_indeed
+WHERE review_count > 5000
+GROUP BY company
+ORDER BY avg_rating DESC;
+
+--ANSWER: General Motors, unilever, Microsoft, Nike,American Express, Kaiser PErmaanent with an average rating of 4.19 stars
+
+--QUESTION 11 Find all the job titles that contain the word ‘Analyst’. How many different job titles are there?
+SELECT COUNT(DISTINCT(title))
+FROM data_analyst_jobs_indeed
+WHERE title ILIKE '%Analyst%'
+
+
+--ANSWER: 774
+
+--QUESTION 12 How many different job titles do not contain either the word ‘Analyst’ or the word ‘Analytics’?
+--What word do these positions have in common?
+SELECT DISTINCT (title) 
+FROM data_analyst_jobs_indeed
+WHERE title NOT LIKE '%Analyst%' 
+AND title NOT LIKE '%Analytics%'
+AND title NOT LIKE '%ANALYST%'
+AND title NOT LIKE '%ANALYTICS%'
+AND title NOT LIKE '%analyst%'
+AND title NOT LIKE '%analytics%'
+
+--OR
+SELECT DISTINCT (title) 
+FROM data_analyst_jobs_indeed
+WHERE title NOT iLIKE '%Analyst%' 
+AND title NOT iLIKE '%Analytics%';
+
+
+
+--ANSWER: the position is for visualization data and most common skill is tableau
+
+-- BONUS: You want to understand which jobs requiring SQL are hard to fill. Find the number of jobs by industry (domain) that require SQL and have been posted longer than 3 weeks.
+
+--Disregard any postings where the domain is NULL.
+--Order your results so that the domain with the greatest number of hard to fill jobs is at the top.
+--Which three industrie)s are in the top 3 on this list? How many jobs have been listed for more than 3 weeks for each of the top 3?
+
+SELECT domain, count(*) AS domain_jobs
+FROM data_analyst_jobs_indeed
+WHERE skill LIKE '%SQL%'
+AND days_since_posting >21
+AND domain IS NOT NULL
+GROUP BY domain
+ORDER BY domain_jobs DESC
+LIMIT 3;
 
